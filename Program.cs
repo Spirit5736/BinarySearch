7cfcf5b45e3d0686491dfd5b2131a66213ac95aa
ivk <ivan.keynes@bbconsult.co.uk>
2022-11-13 03:38:49 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿int? FindValue(int[] list, int item)
+{
+    int lowValue = 0;
+    int highValue = list.Length - 1;
+
+
+    while (lowValue <= highValue)
+    {
+        int midValue = (lowValue + highValue) / 2;
+        int guess = list[midValue];
+
+        if (guess == item)
+        {
+            return midValue;
+        }
+        if (guess < item)
+        {
+            lowValue = midValue + 1;
+        }
+        else
+        {
+            highValue = midValue - 1;
+        }
+    }
+
+    return null;
+
+}
+
+int[] list = { 0, 1, 2, 3, 4, 5, 6, 7, 18, 35, 47, 56, 78, 89, 99 };
+Console.WriteLine(FindValue(list, 35));
