708ae4ba63c25898873332b9b3c935bcac2337f2
ivk <ivan.keynes@bbconsult.co.uk>
2022-11-15 03:39:16 +0600

    replaced

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿int? FindValue(int[] list, int item)
+﻿int[] list = { 0, 1, 2, 3, 4, 5, 6, 7, 18, 35, 47, 56, 78, 89, 99 };
+Console.WriteLine(FindValue(list, 35));
+
+int? FindValue(int[] list, int item)
 {
     int lowValue = 0;
     int highValue = list.Length - 1;
@@ -24,8 +27,4 @@
     }
 
     return null;
-
 }
-
-int[] list = { 0, 1, 2, 3, 4, 5, 6, 7, 18, 35, 47, 56, 78, 89, 99 };
-Console.WriteLine(FindValue(list, 35));
